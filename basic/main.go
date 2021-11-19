e63226fa3d96a4082633f1d82a42b4b3b27dfe98
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-11-19 07:47:50 +0700

    add type demo

--- basic/main.go
+++ basic/main.go
@@ -3,5 +3,5 @@ package main
 import _ "fmt"
 
 func main() {
-	arraysStringDemo5()
+	demoType1()
 }
--- basic/type_demo1.go
+++ basic/type_demo1.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+type ListInt []int
+type ListBool []bool
+type ListString []string
+
+func demoType1()  {
+
+	numbers := ListInt{1,2,3,4}
+	fmt.Println(numbers)
+
+	var names = ListString{"Java", "Kotlin", "Go"}
+	fmt.Println(names)
+
+	var statuses ListBool = ListBool{true, false}
+	fmt.Println(statuses)
+
+}
