366689f9de689027938e2075bf8c8fd6958ec0b1
Pratya Yeekhaday <pratya.yeekhaday@accenture.com>
2021-11-22 01:07:24 +0700

    pointer

--- basic/pointer_demo1.go
+++ basic/pointer_demo1.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+)
+
+func pointerDemo() {
+
+	// var number int = 10
+	number := 10
+	fmt.Println("number", number, &number)
+
+	// var pointerNumber *int = &number
+	pointerNumber := &number
+	fmt.Println("pointerNumber", *pointerNumber, pointerNumber)
+
+	fmt.Println("after pointer change")
+	*pointerNumber = 20
+	fmt.Println("number", number, &number)
+	fmt.Println("pointerNumber", *pointerNumber, pointerNumber)
+
+}
